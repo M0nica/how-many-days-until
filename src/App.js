c64f46520f1564231b3e47df960c0675edfbb08d
rai <zclinux@gmail.com>
2018-10-13 09:25:56 -0400

    remove import not needed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Christmas from "./Components/Christmas";
 import NewYear from "./Components/NewYear";
 import Home from "./Components/Home";
 import { Switch, Route } from "react-router-dom";
-import Navigation from "./Components/Navigation";
 
 class App extends Component {
   render() {
